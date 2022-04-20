0a285ae5a8d2c4f9ba52aef751975fe11eaf814f
Melinda Higgins <melinda.higgins@emory.edu>
2022-04-19 21:59:22 -0400

    update module 01 and readme and script

--- module01_Rscript.R
+++ module01_Rscript.R
@@ -1,4 +1,10 @@
 # MODULE 01 R Script ============================
+# Console Commands Line Examples ================
+4 + 4
+sqrt(25)
+pi
+seq(from=1, to=10, by=0.5)
+
 # EXERCISE 01 ===================================
 # save sequence of numbers in object x
 x <- seq(from=1, to=10, by=0.5)
@@ -27,8 +33,12 @@ plot(x, sin(x))
 # make the color red
 plot(x, sin(x), type = "both", col = "red")
 
-# plot x and the tangent of x
+# [YOUR TURN] plot x and the tangent of x
 # change color to blue
 # hint: help(sin) - to see list of other trig functions
 
-# EXERCISE 03 ===================================
+
+# [YOUR TURN] plot log of x with log y
+# change color to green
+# hint: help(log) - to see list of log functions
+
